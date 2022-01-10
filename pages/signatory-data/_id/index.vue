612c15dcf74fae0bb39337970e20d4fd00b28e02
Mark Brough <mark.brough@gmail.com>
2022-01-10 16:44:24 +0100

    Update to make clear it can refer to activities

--- pages/signatory-data/_id/index.vue
+++ pages/signatory-data/_id/index.vue
@@ -81,7 +81,7 @@
             <hr />
             <h3>IATI Standard v2.03 elements</h3>
             <p class="lead">
-              The below charts show summaries by the total number of transactions.
+              The below charts show summaries by the total number of transactions or activities.
             </p>
           </b-col>
         </b-row>
